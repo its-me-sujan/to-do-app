f7b6515d33c6b8a6e822642ac50a20c9daadd6a3
its-me-sujan <itsmesujangit@gmail.com>
2023-07-09 14:27:00 +0545

    database connection completed

--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
-const PORT = 8000;
-
 const app = express();
+const PORT = 8090;
+mongoose.connect('mongodb://localhost:27017/to-do-app').then(() => {
+    console.log('connected to MongoDb');
+});
 
 const indexRouter = require('./routes');
+
+app.use('/',indexRouter);
 
 app.listen(PORT, () => {
     console.log('server running on port ',PORT);
