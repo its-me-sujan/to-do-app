bd094cf4d0ec848f778bf5ccda6330e35db08602
its-me-sujan <itsmesujangit@gmail.com>
2023-07-09 14:48:35 +0545

    created CRUD housing

---
+++ server/modules/todos/todo.controller.js
@@ -0,0 +1,13 @@
+const TodoModel = require('./todo.model');
+
+const create = (payload) => {};
+
+const list = () => {};
+
+const getById = (id) => {};
+
+const updateById = (id, payload) => {};
+
+const remove = (id) => {};
+
+module.exports = {create, list, getById, updateById, remove};
