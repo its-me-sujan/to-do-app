16da40f0d5bddfb6c69493473891d40398f26f6d
its-me-sujan <itsmesujangit@gmail.com>
2023-07-09 15:07:44 +0545

    added post for create

--- server/modules/subtasks/subtask.routes.api.js
+++ server/modules/subtasks/subtask.routes.api.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const subtaskController = require('./subtask.controller')
 
 router.use('/', (req, res, next) => {
     res.json({msg:"subtasks API"})
+});
+
+router.post('/', async(req, res) => {
+    const subtaskResult = await subtaskController.create(req.body);
+    res.json({data: subtaskResult});
 });
 
 module.exports = router;
