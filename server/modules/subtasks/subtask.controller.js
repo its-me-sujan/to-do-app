69ce793df2690aaa6f52208ef191290c17c24423
its-me-sujan <itsmesujangit@gmail.com>
2023-07-09 14:49:20 +0545

    created CRUD housing

---
+++ server/modules/subtasks/subtask.controller.js
@@ -0,0 +1,13 @@
+const SubtaskModel = require('./subtask.model');
+
+const create = (payload) => {};
+
+const list = () => {};
+
+const getById = (id) => {};
+
+const updateById = (id, payload) => {};
+
+const remove = (id) => {};
+
+module.exports = {create, list, getById, updateById, remove};
