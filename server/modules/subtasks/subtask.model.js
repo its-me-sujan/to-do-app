0d31779b818f28268a226ea5a4f040cfb9ec9cdc
its-me-sujan <itsmesujangit@gmail.com>
2023-07-09 14:44:00 +0545

    added reference to Todo model

--- server/modules/subtasks/subtask.model.js
+++ server/modules/subtasks/subtask.model.js
@@ -5,6 +5,7 @@ const ObjectId = mongoose.Schema.Types;
 const subTaskSchema = mongoose.Schema({
     title: {type: String, required: true},
     status: {type: String, enums: ['pending', 'complete'], default: 'pending'},
+    todo_id: {type: ObjectId, ref: 'Todo'}, //Reference to Todo model
 });
 
 module.exports = mongoose.model('Subtask', subTaskSchema);
