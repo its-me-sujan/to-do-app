39fd52b603a024452e3d8c8a6a746a77c87a6999
its-me-sujan <itsmesujangit@gmail.com>
2023-07-09 14:20:16 +0545

    created routes with todos nad subtasts

--- server/routes/routes.api.js
+++ server/routes/routes.api.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const toDosRouter = require('../modules/toDos/todo.routes.api');
-const subtasks = require('../modules/subtasks/subtask.routes.api');
+const todoRouter = require('../modules/todos/todo.routes.api');
+const subtaskRouter = require('../modules/subtasks/subtask.routes.api');
 
 router.get('/', (req, res, next) => {
     res.json({msg: "Hello from routes API"});
 });
 
-router.use('/toDos', toDosRouter);
-router.use('/subtasks', subtasksRouter);
+router.use('/todos', todoRouter);
+router.use('/subtasks', subtaskRouter);
 
 module.exports = router;
