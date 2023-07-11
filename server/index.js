33f93d421249fc4cf623a45303201aa544603f67
its-me-sujan <itsmesujangit@gmail.com>
2023-07-11 21:14:43 +0545

    commiting

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
-const app = express();
-const PORT = 8090;
 mongoose.connect('mongodb://localhost:27017/to-do-app').then(() => {
     console.log('connected to MongoDb');
 });
 
+const app = express();
 const indexRouter = require('./routes');
+const PORT = 8700;
 
+
+
+app.use(express.json());
 app.use('/',indexRouter);
 
 app.listen(PORT, () => {
     console.log('server running on port ',PORT);
 });    
-  
--- server/routes/routes.api.js
+++ server/routes/routes.api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const todoRouter = require('../modules/todos/todo.routes.api');
+const todoRouter = require('../modules/toDos/todo.routes.api');
 const subtaskRouter = require('../modules/subtasks/subtask.routes.api');
 
 router.get('/', (req, res, next) => {
