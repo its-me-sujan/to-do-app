10b3a872e3d3c1cc49652640c12fdb053a4258a5
its-me-sujan <itsmesujangit@gmail.com>
2023-07-28 14:20:34 +0545

    completed todo routes

--- server/modules/toDos/todo.routes.api.js
+++ server/modules/toDos/todo.routes.api.js
@@ -2,13 +2,34 @@ const express = require('express');
 const router = express.Router();
 const todoController = require('./todo.controller');
 
-router.get('/', (req, res, next) => {
-    res.json({msg:"TODOS API"})
+//read
+router.get('/', async (req, res, next) => {
+    const result = await todoController.list();
+    res.json({data: results});
 });
 
+// create
 router.post('/', async(req, res) => {
     const todoResult = await todoController.create(req.body);
     res.json({data: todoResult});
+});
+
+// read by id
+router.get('/:id', async(req, res) => {
+    const result = await todoController.getById(req.params.id);
+    res.json({data: results});
+});
+
+// update by id
+router.put('/:id', async(req, res) => {
+    const result = await todoController.updateById(req.params.id, req.body);
+    res.json({data: results});
+});
+
+// delete by id
+router.delete('/:id', async(req, res) => {
+    const result = await todoController.remove(req.params.id);
+    res.json({data: results});
 });
 
 module.exports = router;
