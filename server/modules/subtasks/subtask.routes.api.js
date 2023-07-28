1d5602e140048f30f95eaa810c856d931e3a20f4
its-me-sujan <itsmesujangit@gmail.com>
2023-07-28 14:21:49 +0545

    completed subtask routes

--- server/modules/subtasks/subtask.routes.api.js
+++ server/modules/subtasks/subtask.routes.api.js
@@ -2,13 +2,34 @@ const express = require('express');
 const router = express.Router();
 const subtaskController = require('./subtask.controller')
 
-router.get('/', (req, res, next) => {
-    res.json({msg:"subtasks API"})
+//read
+router.get('/', async (req, res, next) => {
+    const result = await subtaskController.list();
+    res.json({data: results});
 });
 
+// create
 router.post('/', async(req, res) => {
-    const subtaskResult = await subtaskController.create(req.body);
-    res.json({data: subtaskResult});
+    const todoResult = await subtaskController.create(req.body);
+    res.json({data: todoResult});
+});
+
+// read by id
+router.get('/:id', async(req, res) => {
+    const result = await subtaskController.getById(req.params.id);
+    res.json({data: results});
+});
+
+// update by id
+router.put('/:id', async(req, res) => {
+    const result = await subtaskController.updateById(req.params.id, req.body);
+    res.json({data: results});
+});
+
+// delete by id
+router.delete('/:id', async(req, res) => {
+    const result = await subtaskController.remove(req.params.id);
+    res.json({data: results});
 });
 
 module.exports = router;
