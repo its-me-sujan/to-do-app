41942f9ed7413dbacb141d6222c90ba8dd3e4354
its-me-sujan <itsmesujangit@gmail.com>
2023-08-01 13:12:21 +0545

    renamed todo to todo_id

--- server/modules/subtasks/subtask.model.js
+++ server/modules/subtasks/subtask.model.js
@@ -5,7 +5,7 @@ const {ObjectId} = mongoose.Schema;
 const subTaskSchema = mongoose.Schema({
     title: {type: String, required: true},
     status: {type: String, enums: ['pending', 'complete'], default: 'pending'},
-    todo: {type: ObjectId, ref: 'Todo'}, //Reference to Todo model
+    todo_id: {type: ObjectId, ref: 'Todo'}, //Reference to Todo model
 });
 
 module.exports = mongoose.model('Subtask', subTaskSchema);
