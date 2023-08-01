d16999edc9e30bc9f8e336cf0cbe3fad9fe199b9
its-me-sujan <itsmesujangit@gmail.com>
2023-08-01 13:12:00 +0545

    renamed result to results

--- server/modules/toDos/todo.routes.api.js
+++ server/modules/toDos/todo.routes.api.js
@@ -4,31 +4,31 @@ const todoController = require('./todo.controller');
 
 //read
 router.get('/', async (req, res, next) => {
-    const result = await todoController.list();
+    const results = await todoController.list();
     res.json({data: results});
 });
 
 // create
 router.post('/', async(req, res) => {
-    const todoResult = await todoController.create(req.body);
-    res.json({data: todoResult});
+    const results = await todoController.create(req.body);
+    res.json({data: results});
 });
 
 // read by id
 router.get('/:id', async(req, res) => {
-    const result = await todoController.getById(req.params.id);
+    const results = await todoController.getById(req.params.id);
     res.json({data: results});
 });
 
 // update by id
 router.put('/:id', async(req, res) => {
-    const result = await todoController.updateById(req.params.id, req.body);
+    const results = await todoController.updateById(req.params.id, req.body);
     res.json({data: results});
 });
 
 // delete by id
 router.delete('/:id', async(req, res) => {
-    const result = await todoController.remove(req.params.id);
+    const results = await todoController.remove(req.params.id);
     res.json({data: results});
 });
 
