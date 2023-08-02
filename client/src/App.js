e2ef014601d554558c3d5ed1b736c1994e2cfb59
its-me-sujan <itsmesujangit@gmail.com>
2023-08-02 13:58:18 +0545

    added goal component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import Title from "./components/Title";
 import Example from './components/Example';
 import Garage from "./components/Garage";
+import Goal from "./components/Goal";
 function App() {
   const cars = ['Ford', 'BMW', 'Audi'];
   return <>
     <>
       <Title name="TODO App"/>
-      <Example isGoal = {true}/>
+      <Example isGoal = {false}/>
       <Garage cars = {cars} />
+      <Goal isGoal = {true} />
     </>
   </>;
 }
