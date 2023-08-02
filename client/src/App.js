e63617ff1618bc75aa1f0076ebe974c9525534b7
its-me-sujan <itsmesujangit@gmail.com>
2023-08-02 13:32:40 +0545

    passed isGoal props

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import Title from "./components/Title";
-import Element from './components/Element';
+import Example from './components/Example';
 function App() {
   return <>
     <>
       <Title name="TODO App"/>
-      <Element />
+      <Example isGoal = {true}/>
     </>
   </>;
 }
