7346501728846f1a8a1ed5c9bcb1e5d13ec5e29e
its-me-sujan <itsmesujangit@gmail.com>
2023-08-02 13:52:05 +0545

    passed array props

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import Title from "./components/Title";
 import Example from './components/Example';
+import Garage from "./components/Garage";
 function App() {
+  const cars = ['Ford', 'BMW', 'Audi'];
   return <>
     <>
       <Title name="TODO App"/>
       <Example isGoal = {true}/>
+      <Garage cars = {cars} />
     </>
   </>;
 }
