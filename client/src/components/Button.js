04de0ecf526f2b4fa64ae43db27115ba3e7d59f6
its-me-sujan <itsmesujangit@gmail.com>
2023-08-03 15:20:12 +0545

    project cleanup

--- client/src/components/Button.js
+++
@@ -1,6 +0,0 @@
-import { Button } from 'react-bootstrap';
-const ButtonComponent = () => {
-    return <div><Button variant="primary">Primary</Button>{' '}</div>
-};
-
-export default ButtonComponent;
--- client/src/components/Car.js
+++
@@ -1,5 +0,0 @@
-export default function Car(props) {
-  return (
-    <li>I am a {props.brand}</li>
-  )
-}
