b25e664bea230624206b01e8d18c71e2c722a3be
its-me-sujan <itsmesujangit@gmail.com>
2023-08-03 12:57:01 +0545

    used useEffect hooks for first render

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -7,7 +7,7 @@ export default function Timer() {
         setTimeout(() => {
             setCount((count) => count+1);
         },1000);
-    });
+    }, []);
   return (
     <h1>I've rendered {count} times!</h1>
   )
