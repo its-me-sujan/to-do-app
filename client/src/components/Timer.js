3804d2e1f3e967dc2b41f63d909e8d46f9c205d9
its-me-sujan <itsmesujangit@gmail.com>
2023-08-03 13:02:39 +0545

    useEffect cleanup

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -4,9 +4,11 @@ export default function Timer() {
     const [count, setCount] = useState(0);
     
     useEffect(()=>{
-        setTimeout(() => {
+        let timer = setTimeout(() => {
             setCount((count) => count+1);
         },1000);
+        
+        return () => clearTimeout(timer);
     }, []);
   return (
     <h1>I've rendered {count} times!</h1>
