4da537a4368a9f97400bb2ca7710535a2a518619
its-me-sujan <itsmesujangit@gmail.com>
2023-08-03 13:50:39 +0545

    added fetchdata component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Goal from "./components/Goal";
 import FavoriteColor from "./components/FavoriteColor";
 import CarHook from "./components/CarHook";
 import Timer from "./components/Timer";
+import FetchData from "./components/FetchData";
 
 function App() {
   const cars = ['Ford', 'BMW', 'Audi'];
@@ -17,6 +18,7 @@ function App() {
       <FavoriteColor />
       <CarHook />
       <Timer />
+      <FetchData />
     </>
   </>;
 }
