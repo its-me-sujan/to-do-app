57eb13e2140a54fdbe0a1e816878c1abea59a0cc
its-me-sujan <itsmesujangit@gmail.com>
2023-08-03 15:30:02 +0545

    project cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import Title from "./components/Title";
-// import Example from './components/Example';
-// import Garage from "./components/Garage";
-// import Goal from "./components/Goal";
-// import FavoriteColor from "./components/FavoriteColor";
-// import CarHook from "./components/CarHook";
-// import Timer from "./components/Timer";
-// import FetchData from "./components/FetchData";
-import CustomHook from "./components/CustomHook";
 import { Col,Container, Row } from "react-bootstrap";
-
+import Title from "./components/Title";
+import AddTodo from "./components/AddTodo";
+import Todo from "./pages/Todo"
 
 function App() {
-  // const cars = ['Ford', 'BMW', 'Audi'];
   return <>
     <>
       <Container>
         <Row>
             <Col md={{span: 6, offset: 3}}>
               <Title name = "TODO App"/>
-              <CustomHook />
+              <AddTodo label = "Add New Task" placeholder = "Eg: Do Homework" />
+              <Todo />
             </Col>
         </Row>
 
