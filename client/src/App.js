bb4b5761e10a7b9a570a6fbf1f3a35c6c0522dc3
its-me-sujan <itsmesujangit@gmail.com>
2023-08-03 14:32:37 +0545

    project cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import Title from "./components/Title";
-import Example from './components/Example';
-import Garage from "./components/Garage";
-import Goal from "./components/Goal";
-import FavoriteColor from "./components/FavoriteColor";
-import CarHook from "./components/CarHook";
-import Timer from "./components/Timer";
-import FetchData from "./components/FetchData";
+// import Example from './components/Example';
+// import Garage from "./components/Garage";
+// import Goal from "./components/Goal";
+// import FavoriteColor from "./components/FavoriteColor";
+// import CarHook from "./components/CarHook";
+// import Timer from "./components/Timer";
+// import FetchData from "./components/FetchData";
+import CustomHook from "./components/CustomHook";
+import { Col,Container, Row } from "react-bootstrap";
+
 
 function App() {
-  const cars = ['Ford', 'BMW', 'Audi'];
+  // const cars = ['Ford', 'BMW', 'Audi'];
   return <>
     <>
-      <Title name="TODO App"/>
-      <Example isGoal = {false}/>
-      <Garage cars = {cars} />
-      <Goal isGoal = {true} />
-      <FavoriteColor />
-      <CarHook />
-      <Timer />
-      <FetchData />
+      <Container>
+        <Row>
+            <Col md={{span: 6, offset: 3}}>
+              <Title name = "TODO App"/>
+              <CustomHook />
+            </Col>
+        </Row>
+
+      </Container>
     </>
   </>;
 }
