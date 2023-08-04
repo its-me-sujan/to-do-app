18b99b612c9244be85d8c4d3f0b7f871a0084655
its-me-sujan <itsmesujangit@gmail.com>
2023-08-04 13:36:18 +0545

    added more design

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -1,20 +1,45 @@
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
+import { Form, InputGroup, Button } from "react-bootstrap";
+import { useState, useContext } from "react";
+import { DataContext } from "../contexts";
+
+import MsgAlert from "./Alert";
 
 function AddTodo(props) {
-    const {label,  placeholder} = props;
-    return (
-        <>
-            <InputGroup className="mb-3">
-            <InputGroup.Text>{label || "Label"}</InputGroup.Text>
-            <Form.Control placeholder= {placeholder || "Placeholder"}/>
-            <Button variant="outline-secondary" id="button-addon2">
-            Submit
-            </Button>
-        </InputGroup>
-        </>
-    )
+  const { post, loading, error } = useContext(DataContext);
+  const { url, label, placeholder } = props;
+  const [task, setTask] = useState({
+    title: "",
+  });
+
+  const handleClick = async () => {
+    const payload = task;
+    await post(url, payload);
+    setTask((prev) =>{
+        return { ...prev, title:""};
+    });
+  };
+
+  return (
+    <>
+      <InputGroup className="mb-3">
+        {loading && <div>Loading...</div>}
+        {error && <MsgAlert msg={error?.message} />}
+        <InputGroup.Text>{label || "Label"}</InputGroup.Text>
+        <Form.Control
+          placeholder={placeholder || "Placeholder"}
+          value={task?.title}
+          onChange={(e) =>
+            setTask((prev) => {
+              return { ...prev, title: e.target.value };
+            })
+          }
+        />
+        <Button variant="outline-secondary" onClick={handleClick}>
+          Submit
+        </Button>
+      </InputGroup>
+    </>
+  );
 }
 
 export default AddTodo;
