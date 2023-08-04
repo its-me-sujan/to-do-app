f64fc2dd613f0feb7664779bb06b6e7bdab174d0
its-me-sujan <itsmesujangit@gmail.com>
2023-08-04 13:09:45 +0545

    added constants

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import { Col,Container, Row } from "react-bootstrap";
-import Title from "./components/Title";
 import AddTodo from "./components/AddTodo";
-import Todo from "./pages/Todo"
-import ListTodo from "./components/ListTodo";
+import {API_SERVER} from "./constants";
+import Title from "./components/Title";
+import Todo from "./pages/Todo";
+
 function App() {
   return <>
     <>
@@ -10,12 +11,10 @@ function App() {
         <Row>
             <Col md={{span: 6, offset: 3}}>
               <Title name = "TODO App"/>
-              <AddTodo label = "Add New Task" placeholder = "Eg: Do Homework" />
+              <AddTodo label = "Add New Task" placeholder = "Eg: Do Homework" url={`${API_SERVER}/todos`}/>
               <Todo />
-              <ListTodo />
             </Col>
         </Row>
-
       </Container>
     </>
   </>;
