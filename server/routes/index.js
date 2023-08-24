c921ac9938cdcb907e95a29b17b35275903ba4f6
its-me-sujan <itsmesujangit@gmail.com>
2023-08-24 11:39:03 +0545

    minor changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const apiRouter = require("./routes.api");
-router.get("/", (req, res) => {
-  res.send("App is running");
-});
 router.use("/api/v1", apiRouter);
 
 module.exports = router;
