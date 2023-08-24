edaacef6412f1d7def6346b9a4ee53655756bd5b
its-me-sujan <itsmesujangit@gmail.com>
2023-08-24 11:41:30 +0545

    added response

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const apiRouter = require("./routes.api");
+router.get("/", (req, res) => {
+  res.send("App is running");
+});
 router.use("/api/v1", apiRouter);
 
 module.exports = router;
